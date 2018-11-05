3ad9626ab8fc5d9280697dd6f7acc7bbdb378de1
Mohamed <mohamed.a.dawil@outlook.com>
2018-11-05 22:35:42 +0100

    added develop and created a vs file

---
+++ Checkpoint/Checkpoint/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Checkpoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
