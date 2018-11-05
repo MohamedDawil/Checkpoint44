cc9f2c2f2a7eb1c905b9eb71630b56b0c1d94c58
Mohamed <mohamed.a.dawil@outlook.com>
2018-11-05 22:51:52 +0100

    added fifth movie

--- Checkpoint/Checkpoint/Program.cs
+++ Checkpoint/Checkpoint/Program.cs
@@ -8,6 +8,7 @@ namespace Checkpoint
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Best movies ever!");
+            Console.WriteLine("Terminal Death");
         }
     }
 }
